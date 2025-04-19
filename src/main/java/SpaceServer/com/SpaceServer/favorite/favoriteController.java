d35e365941dafc03a13d84aadc168c42aef510f0
baesaa0304 <baesaa0304@naver.com>
2025-04-20 08:45:31 +0900

    feat: 테스트 확인

--- src/main/java/SpaceServer/com/SpaceServer/favorite/favoriteController.java
+++ src/main/java/SpaceServer/com/SpaceServer/favorite/favoriteController.java
@@ -1,26 +1,27 @@
 package SpaceServer.com.SpaceServer.favorite;
 
 import SpaceServer.com.SpaceServer.favorite.service.FavoriteService;
+import SpaceServer.com.SpaceServer.security.jwt.JwtTokenProvider;
 import SpaceServer.com.SpaceServer.stars.dto.response.StarsListResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/my")
 @RequiredArgsConstructor
 public class favoriteController {
     private final FavoriteService favoriteService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @GetMapping("/favorites")
     public ResponseEntity<Page<StarsListResponse>> getMyFavorites(
-            @RequestParam String userId,
+            @RequestHeader("Authorization") String accessToken,
             @RequestParam(defaultValue = "0") int page
     ) {
+        String pureToken = accessToken.replace("Bearer ", "");
+        String userId = jwtTokenProvider.getUserIdFromToken(pureToken);
         Page<StarsListResponse> favorites = favoriteService.getFavoritesByUserId(userId, page);
         return ResponseEntity.ok(favorites);
     }
