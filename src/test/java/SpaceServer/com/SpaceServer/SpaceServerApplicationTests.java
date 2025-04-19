58e03f902451627bd77ebd89aed164f9825b8a47
baesaa0304 <baesaa0304@naver.com>
2025-04-19 20:23:26 +0900

    feat : 서버 환경설정

---
+++ src/test/java/SpaceServer/com/SpaceServer/SpaceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package SpaceServer.com.SpaceServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
