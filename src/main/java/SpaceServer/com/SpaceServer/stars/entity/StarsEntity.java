99125918b7356b2f300801bd608dd71bbf46a31e
baesaa0304 <baesaa0304@naver.com>
2025-04-19 22:42:44 +0900

    fix : 별자리 이미지 저장

--- src/main/java/SpaceServer/com/SpaceServer/stars/entity/StarsEntity.java
+++ src/main/java/SpaceServer/com/SpaceServer/stars/entity/StarsEntity.java
@@ -21,28 +21,18 @@ public class StarsEntity {
     @Column(name = "image_url", nullable = false)
     private String imageUrl; // 별자리 이미지 URL
 
-    @Column(name = "major_star_1", nullable = false)
-    private String majorStar1; // 주요 별 1
-
-    @Column(name = "major_star_2", nullable = false)
-    private String majorStar2; // 주요 별 2
-
-    @Column(name = "major_star_3", nullable = false)
-    private String majorStar3; // 주요 별 3
+    @Column(name = "major_star", nullable = false)
+    private String majorStar; // 주요 별 1
 
     @Column(name = "description", nullable = false, columnDefinition = "TEXT")
     private String description; // 별자리 설명 (TEXT)
 
 
     @Builder
-    public StarsEntity(String name, String imageUrl,
-                               String majorStar1, String majorStar2, String majorStar3,
-                               String description) {
+    public StarsEntity(String name, String imageUrl, String majorStar, String description) {
         this.name = name;
         this.imageUrl = imageUrl;
-        this.majorStar1 = majorStar1;
-        this.majorStar2 = majorStar2;
-        this.majorStar3 = majorStar3;
+        this.majorStar = majorStar;
         this.description = description;
     }
 }
