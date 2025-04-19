0f6cf6516f3ef4a80c16ddafb3e373dff58be223
baesaa0304 <baesaa0304@naver.com>
2025-04-20 02:55:51 +0900

    feat : 이미지 추가, ai 기능 추가

--- src/main/java/SpaceServer/com/SpaceServer/image/ProfileImageUtil.java
+++ src/main/java/SpaceServer/com/SpaceServer/image/ProfileImageUtil.java
@@ -8,9 +8,8 @@ import java.util.Random;
 @Component
 public class ProfileImageUtil {
     private static final List<String> PROFILE_IMAGES = List.of(
-            "https://your-bucket.s3.ap-northeast-2.amazonaws.com/profile1.png",
-            "https://your-bucket.s3.ap-northeast-2.amazonaws.com/profile2.png",
-            "https://your-bucket.s3.ap-northeast-2.amazonaws.com/profile3.png"
+            "https://profilehackton.s3.ap-northeast-2.amazonaws.com/profile1.png",
+            "https://profilehackton.s3.ap-northeast-2.amazonaws.com/profile2.png"
     );
 
     public String getRandomProfileUrl() {
